cd0e198fd08e2448ee22b9f2346f04cff34120da
SupitS <84420067+SupitS@users.noreply.github.com>
2021-06-04 22:49:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// This is my first JavaScript code
+console.log('Hello Winc Academy');
+
+let name = 'Stenny';
+console.log(name);
+
+let number = 4 + 4;
+console.log(number);
+
+let number2 = '4+4';
+console.log(number2);
+
+console.log(10 + 2);
+console.log(10 - 2);
+console.log(10 * 2);
+console.log(10 / 2);
+console.log(10 % 4);
+
+let age = 30;
+console.log(age);
+
+let age1 = '30';
+console.log(age1);
+
